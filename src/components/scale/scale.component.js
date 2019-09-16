36955c71f2a5b6504ff5a3a36ad79d0c1df2226a
rnormando <rodrigo.normando@omnibees.com>
2019-09-16 12:53:49 -0300

    ::

---
+++ src/components/scale/scale.component.js
@@ -0,0 +1,205 @@
+import React, { useRef, useEffect } from 'react';
+import './scale.component.scss';
+import scaleNotes from './scale.constants';
+
+
+function Scale(props) {
+
+    const scaleHtml = [
+        {
+            id: 'C-a',
+            src: scaleNotes.firstC,
+            className: 'scale__white-keys',
+            hasChild: true,
+            child: {
+                id: 'C#-w',
+                src: scaleNotes.firstCSharp,
+                className: 'scale__black-keys',
+            }
+        },
+        {
+            id: 'D-s',
+            src: scaleNotes.firstD,
+            className: 'scale__white-keys',
+            hasChild: true,
+            child: {
+                id: 'D#-e',
+                src: scaleNotes.firstDSharp,
+                className: 'scale__black-keys',
+            }
+        },
+        {
+            id: 'E-d',
+            src: scaleNotes.firstE,
+            className: 'scale__white-keys',
+            hasChild: false,
+        },
+        {
+            id: 'F-f',
+            src: scaleNotes.firstF,
+            className: 'scale__white-keys',
+            hasChild: true,
+            child: {
+                id: 'C#-t',
+                src: scaleNotes.firstFSharp,
+                className: 'scale__black-keys',
+            }
+        },
+        {
+            id: 'G-g',
+            src: scaleNotes.firstG,
+            className: 'scale__white-keys',
+            hasChild: true,
+            child: {
+                id: 'G#-y',
+                src: scaleNotes.firstGSharp,
+                className: 'scale__black-keys',
+            }
+        },
+        {
+            id: 'A-h',
+            src: scaleNotes.firstA,
+            className: 'scale__white-keys',
+            hasChild: true,
+            child: {
+                id: 'A#-u',
+                src: scaleNotes.firstASharp,
+                className: 'scale__black-keys',
+            }
+        },
+        {
+            id: 'B-j',
+            src: scaleNotes.firstB,
+            className: 'scale__white-keys',
+            hasChild: false
+        },
+        // {
+        //     id: 'C2-b',
+        //     src: scaleNotes.secondC,
+        //     className: 'scale__white-keys',
+        //     hasChild: true,
+        //     child: {
+        //         id: 'C2#-u',
+        //         src: scaleNotes.secondCSharp,
+        //         className: 'scale__black-keys',
+        //     }
+        // },
+        // {
+        //     id: 'D2-n',
+        //     src: scaleNotes.secondD,
+        //     className: 'scale__white-keys',
+        //     hasChild: true,
+        //     child: {
+        //         id: 'D2#-i',
+        //         src: scaleNotes.secondDSharp,
+        //         className: 'scale__black-keys',
+        //     }
+        // },
+        // {
+        //     id: 'E2-m',
+        //     src: scaleNotes.secondE,
+        //     className: 'scale__white-keys',
+        //     hasChild: false,
+        // },
+        // {
+        //     id: 'F2-,',
+        //     src: scaleNotes.secondF,
+        //     className: 'scale__white-keys',
+        //     hasChild: true,
+        //     child: {
+        //         id: 'F2#-o',
+        //         src: scaleNotes.secondFSharp,
+        //         className: 'scale__black-keys',
+        //     }
+        // },
+        // {
+        //     id: 'G2-.',
+        //     src: scaleNotes.secondG,
+        //     className: 'scale__white-keys',
+        //     hasChild: true,
+        //     child: {
+        //         id: 'G2#-p',
+        //         src: scaleNotes.secondGSharp,
+        //         className: 'scale__black-keys',
+        //     }
+        // },
+        // {
+        //     id: 'A2-;',
+        //     src: scaleNotes.secondA,
+        //     className: 'scale__white-keys',
+        //     hasChild: true,
+        //     child: {
+        //         id: 'A2#-/',
+        //         src: scaleNotes.secondASharp,
+        //         className: 'scale__black-keys',
+        //     }
+        // },
+        // {
+        //     id: 'A2-enter',
+        //     src: scaleNotes.secondB,
+        //     className: 'scale__white-keys',
+        //     hasChild: false,
+        // },
+    ];
+
+    let scale = useRef(null);
+
+    const play = (e, releaseMe) => {
+        debugger
+        for (let i = 0; i < scale.current.children.length; i++ ) {
+            if (scale.current.children[i].id.split('-')[1] === e.key) {
+                if (releaseMe) {
+                    scale.current.children[i].children[0] && handleAudio(scale.current.children[i].children[0], true)
+                } else {
+                    scale.current.children[i].children[0] && handleAudio(scale.current.children[i].children[0]);
+                }
+            } else if (scale.current.children[i].children[1] && scale.current.children[i].children[1].id.split('-')[1] === e.key) {
+                if (releaseMe) {
+                    handleAudio(scale.current.children[i].children[1].children[0], true, true);
+                } else {
+                    handleAudio(scale.current.children[i].children[1].children[0], false, true);
+                }
+            }
+        }
+        console.log('-->', scale);
+    }
+
+    const handleAudio = (element, pause, blackKeys) => {
+        if (pause) {
+            element.load();
+            blackKeys ? element.parentElement.classList.remove('scale__black-keys-pressed') : element.parentElement.classList.remove('scale__white-keys-pressed');
+        } else {
+            element.play();
+            blackKeys ? element.parentElement.classList.add('scale__black-keys-pressed') : element.parentElement.classList.add('scale__white-keys-pressed');
+        }
+    }
+
+    const releaseNote = (e) => {
+        play(e, true);
+    }
+
+    useEffect(() =>  {
+        document.addEventListener('keydown', play);
+        document.addEventListener('keyup', releaseNote);
+
+    });
+    
+    return (
+            <div className="scale" ref={scale}>
+                {scaleHtml.map(element => (
+                    <div id={element.id} className={element.className} key={element.id}>
+                        <audio src={element.src} type="audio/mpeg"></audio>
+                        {element.hasChild && (
+                            <div id={element.child.id} className={element.child.className}>
+                                <audio>
+                                    <source src={element.child.src} type="audio/mpeg" />
+                                </audio>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        )
+}
+
+export default Scale;
---
+++ src/components/scale/scale.constants.js
@@ -0,0 +1,53 @@
+import firstC from '../../assets/piano-keys/1_c_1.mp3';
+import firstCSharp from '../../assets/piano-keys/mid_c_sharp.mp3';
+import firstD from '../../assets/piano-keys/2_d_2.mp3';
+import firstDSharp from '../../assets/piano-keys/mid_d_sharp.mp3';
+import firstE from '../../assets/piano-keys/3_e_3.mp3';
+import firstF from '../../assets/piano-keys/4_f_4.mp3';
+import firstFSharp from '../../assets/piano-keys/mid_f_sharp.mp3';
+import firstG from '../../assets/piano-keys/5_g_5.mp3'
+import firstGSharp from '../../assets/piano-keys/mid_g_sharp.mp3';
+import firstA from '../../assets/piano-keys/6_a_6.mp3';
+import firstASharp from '../../assets/piano-keys/mid_a_sharp.mp3';
+import firstB from '../../assets/piano-keys/7_b_7.mp3';
+import secondC from '../../assets/piano-keys/high_c.mp3';
+import secondCSharp from '../../assets/piano-keys/high_c_sharp.mp3';
+import secondD from '../../assets/piano-keys/high_d.mp3';
+import secondDSharp from '../../assets/piano-keys/high_d_sharp.mp3';
+import secondE from '../../assets/piano-keys/high_e.mp3';
+import secondF from '../../assets/piano-keys/high_f.mp3';
+import secondFSharp from '../../assets/piano-keys/high_f_sharp.mp3';
+import secondG from '../../assets/piano-keys/high_g.mp3';
+import secondGSharp from '../../assets/piano-keys/high_g_sharp.mp3';
+import secondA from '../../assets/piano-keys/high_a.mp3';
+import secondASharp from '../../assets/piano-keys/high_a_sharp.mp3';
+import secondB from '../../assets/piano-keys/high_b.mp3';
+
+const scaleNotes = {
+    firstC,
+    firstCSharp,
+    firstD,
+    firstDSharp,
+    firstE,
+    firstF,
+    firstFSharp,
+    firstG,
+    firstGSharp,
+    firstA,
+    firstASharp,
+    firstB,
+    secondC,
+    secondCSharp,
+    secondD,
+    secondDSharp,
+    secondE,
+    secondF,
+    secondFSharp,
+    secondG,
+    secondGSharp,
+    secondA,
+    secondASharp,
+    secondB
+}
+
+export default scaleNotes;
